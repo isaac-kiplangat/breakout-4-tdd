7ffdb007543e732fb1aeefb554edcc3bc21b44dd
5000vw <isaackiplangat1999@gmail.com>
2023-11-30 16:52:51 +0300

    first commit

---
+++ src/bst.js
@@ -0,0 +1,25 @@
+export default class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insertNode(node) {
+    this.root = node;
+  }
+  insert(node) {
+    if (this.root === null) {
+      this.root = node;
+    } else {
+      this.root.left = node;
+    }  
+  }
+  insert(node) {
+    if (this.root === null) {
+      this.root = node;
+    } else if (this.root.data > node.data) {
+      this.root.left = node;
+    } else if (this.root.data < node.data) {
+      this.root.right = node;
+    }
+  }
+}
